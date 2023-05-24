9c1c02dc67b89e7fdfbb9bfafd975933b9eae045
jfranklin12 <j.franks725@gmail.com>
2023-05-23 18:49:34 -0700

    random quote initial state set

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.scss';
+import React, {useState} from 'react';
+const Quote = require('inspirational-quotes');
+
+
 
 function App() {
+  
+
+  const [quote, setQuote] = useState(Quote.getQuote());
+
+
   return (
     <div className='App'>
       <div id='quote-box'>
-        <p id='text'>Quote Text</p>
-        <p id='author'>Quote Author</p>
-        <button id='new-quote'>New Quote</button>
+        <p id='text'>{quote.text}</p>
+        <p id='author'>{quote.author}</p>
+        <button id='new-quote' onClick={setQuote}>New Quote</button>
         <a id='tweet-quote' target='blank' href='twitter.com/intent/tweet'>Tweet Quote</a>
       </div>
     </div>
