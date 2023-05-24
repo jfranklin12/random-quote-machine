8d24dba3c320fda7b2415467478cfb4141943a6c
jfranklin12 <j.franks725@gmail.com>
2023-05-23 22:19:01 -0700

    random quote is generated on button click and quote data added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './App.scss';
 import React, {useState} from 'react';
-const Quote = require('inspirational-quotes');
+import quotes from './quotes';
 
 
 
 function App() {
   
+  const randomQuote = () => quotes[Math.floor(Math.random()*quotes.length)];
 
-  const [quote, setQuote] = useState(Quote.getQuote());
-
+  const [quote, setQuote] = useState(randomQuote);
 
   return (
     <div className='App'>
       <div id='quote-box'>
         <p id='text'>{quote.text}</p>
-        <p id='author'>{quote.author}</p>
-        <button id='new-quote' onClick={setQuote}>New Quote</button>
+        <p id='author'>{quote.from}</p>
+        <button id='new-quote' onClick={() => setQuote(randomQuote)}>New Quote</button>
         <a id='tweet-quote' target='blank' href='twitter.com/intent/tweet'>Tweet Quote</a>
       </div>
     </div>
