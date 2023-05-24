fc46c985d0798e1bc5a1f8467f4ca0a15f47ad86
jfranklin12 <j.franks725@gmail.com>
2023-05-23 17:28:56 -0700

    initial page layout created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Quote Machine
-        </p>
-
-      </header>
+    <div className='App'>
+      <div id='quote-box'>
+        <p id='text'>Quote Text</p>
+        <p id='author'>Quote Author</p>
+        <button id='new-quote'>New Quote</button>
+        <a id='tweet-quote' target='blank' href='twitter.com/intent/tweet'>Tweet Quote</a>
+      </div>
     </div>
   );
 }
